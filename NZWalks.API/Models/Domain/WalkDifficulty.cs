93a6a3e58180c88426df10d45c4a4f2523a6805a
MarkOrtiz <ortizmark@hotmail.com>
2022-08-14 23:56:50 -0700

    End of Section 2

---
+++ NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public int Code { get; set; }
+
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks")));
+
 
 var app = builder.Build();
 
